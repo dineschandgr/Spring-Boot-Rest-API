c0f75a62b3b34089faa2690919ca901e91fea2d1
Dineshchand Geetha Ravichandran <>
2025-10-11 15:22:02 +0800

    cors

--- src/main/java/com/Rest/API/web/AddressController.java
+++ src/main/java/com/Rest/API/web/AddressController.java
@@ -18,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @RestController
 @RequestMapping("address")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AddressController {
 
     @Autowired
--- src/main/java/com/Rest/API/web/EmployeeController.java
+++ src/main/java/com/Rest/API/web/EmployeeController.java
@@ -5,6 +5,7 @@ import com.Rest.API.model.Employee;
 import com.Rest.API.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("employee")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
 
     @Autowired
